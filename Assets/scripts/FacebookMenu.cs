5730590fd50ad92df36999958c03ac55693a075d
Peter Bue <PetergGrantBue@gmail.com>
2015-05-06 17:03:45 -0700

    init commit

---
+++ Assets/scripts/FacebookMenu.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook;
+using Action = System.Action;
+
+public class FacebookMenu : MonoBehaviour
+{
+	public GameObject facebookPanel;
+	public Image facebookProfilePicture;
+	public Image facebookLoginButton;
+	public Text facebookUsername;
+
+	public static event Action OnLoggedIn;
+
+
+	private Dictionary<string, string> profile = null;
+
+	void Awake()
+	{
+		FB.Init(SetInit);
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void SetInit()
+	{
+		enabled = true;
+	}
+
+	public void FacebookLogin()
+	{
+		FB.Login("public_profile,user_friends,email,publish_actions", LoginCallback);
+	}
+
+	public void LoginCallback(FBResult result)
+	{
+		if (FB.IsLoggedIn)
+		{
+			facebookLoginButton.enabled = false;
+			facebookPanel.SetActive(true);
+			FB.API("/v2.3/me?fields=id, first_name, last_name", HttpMethod.GET, APICallBack);
+		}
+		if (OnLoggedIn != null)
+		{
+			OnLoggedIn();
+		}
+	}
+
+	private void APICallBack(FBResult result)
+	{
+		if (result.Error != null)
+		{
+			Debug.Log("Facebook API Call failed!");
+			return;
+		}
+		Debug.Log(result.Text);
+	}
+
+	private IEnumerator GetProfilePicture()
+	{
+		yield return null;
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		if (!isGameShown)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
---
+++ Assets/scripts/Util.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.MiniJSON;
+
+public static class Util
+{
+	public static Dictionary<string, string> DeserializeJSONProfile(string response)
+	{
+		var responseObject = (Dictionary<string, object>)Json.Deserialize(response);
+		object value;
+		var profile = new Dictionary<string, string>();
+		foreach (var key in responseObject.Keys)
+		{
+			if (responseObject.TryGetValue(key, out value))
+			{
+				profile[key] = (string)value;
+			}
+		}
+		return profile;
+	}
+}
